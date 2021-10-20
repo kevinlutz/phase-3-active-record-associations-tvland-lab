8fb8102fb22c6c8b193fa69e979d5756b5765bbd
+ <+>
2021-10-20 13:44:30 -0400

    commit tree a0eab7ee232d7536b64686f49c9cc93ddb4cdf99

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,13 @@
 class Network < ActiveRecord::Base
-  
+  has_many :shows
+
+  def shows_list
+    self.shows.map do |show|
+        show.name
+    end
+  end
+
+  def sorry
+    "We're sorry about passing on John Mulaney's pilot"
+  end
 end
--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,7 @@
 class AddCatchphraseToCharacters < ActiveRecord::Migration[6.1]
-  def change
-    # Your code here
+  
+    def change
+      add_column :characters, :actor_id, :show_id, :catchphrase
+    end
   end
 end
